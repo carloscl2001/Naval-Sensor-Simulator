47b7e02c40179efb9e1bedb07dd0ccdd0d95a128
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2025-08-23 00:20:40 +0200

    Update qt_observer.h

--- src/qt_observer.h
+++ src/qt_observer.h
@@ -5,18 +5,17 @@
 #include <QTableWidget>
 #include <QListWidget>
 #include <QString>
-#include <QHeaderView>  
+#include <QColor>
 #include <unordered_map>
 
 class QtObserver : public Observer {
 private:
     QTableWidget* table;
-    QListWidget* anomalyList;
+    QListWidget* anomaliesList;
     std::unordered_map<std::string, int> rowMap; // Map sensor name -> row index
-
 public:
-    QtObserver(QTableWidget* tableWidget, QListWidget* anomalyWidget)
-        : table(tableWidget), anomalyList(anomalyWidget) {}
+    QtObserver(QTableWidget* tableWidget, QListWidget* anomaliesWidget)
+        : table(tableWidget), anomaliesList(anomaliesWidget) {}
 
     void registerSensor(const std::string& name, int row) {
         rowMap[name] = row;
@@ -26,16 +25,17 @@ public:
     void update(const std::string& sensorName, double value, bool anomaly) override {
         int row = rowMap[sensorName];
         QString valStr = QString::number(value, 'f', 2);
+
         QTableWidgetItem* item = new QTableWidgetItem(valStr);
-        item->setTextAlignment(Qt::AlignCenter);
 
         if (anomaly) {
             item->setBackground(Qt::red);
-            item->setForeground(Qt::white);
-            anomalyList->addItem(QString::fromStdString(sensorName + " = " + std::to_string(value)));
+            item->setToolTip("Value exceeds threshold!");
+            anomaliesList->addItem(
+                QString("Anomaly detected: %1 = %2").arg(QString::fromStdString(sensorName)).arg(valStr)
+            );
         } else {
             item->setBackground(Qt::green);
-            item->setForeground(Qt::black);
         }
 
         table->setItem(row, 1, item);
